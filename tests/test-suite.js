0d1c9e436564efde97c2ea9dde296711fd59e9e7
ZakarFin <zakarfin@gmail.com>
2015-02-02 22:04:05 +0200

    Figured out the problem with requirejs text plugin in node env. Bundled plugins are too old, fix has been implemented 1,5 years ago on original plugin...

--- tests/test-suite.js
+++ tests/test-suite.js
@@ -7,10 +7,13 @@ define([
         require.config({
             paths : {
                 //create alias to plugins (not needed if plugins are on the baseUrl)
+                text: 'bower_components/requirejs-text/text',
+                // text needs to be newer version than plugins provide
+                // otherwise it will break node (intern) environment since it doesn't handle error callback
+                // the try/catch is the key here: https://github.com/requirejs/text/commit/4ec1d5d5694ac589b0c56fb806195aa6fbf256da
                 async: 'bower_components/requirejs-plugins/src/async',
                 font: 'bower_components/requirejs-plugins/src/font',
                 image: 'bower_components/requirejs-plugins/src/image',
-                text: 'bower_components/requirejs-plugins/lib/text',
                 json: 'bower_components/requirejs-plugins/src/json',
                 mdown: 'bower_components/requirejs-plugins/src/mdown',
                 propertyParser : 'bower_components/requirejs-plugins/src/propertyParser',
